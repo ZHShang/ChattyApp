2197b867a4ebde9849f7e4b1468db51dbec9d2a4
ZHShang <zhihengshang@hotmail.com>
2019-06-04 19:50:52 +0000

    Made the message list work with the sample messages

--- src/ChatBar.jsx
+++ src/ChatBar.jsx
@@ -1,2 +1,13 @@
 import React, {Component} from 'react';
 
+export default class CharBar extends Component {
+  render(){
+    return (
+      <footer class="chatbar">
+        <input class="chatbar-username" placeholder="Your Name (Optional)" />
+        <input class="chatbar-message" placeholder="Type a message and hit ENTER" />
+      </footer>
+      );
+  }
+
+}
