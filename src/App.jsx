dd0a14a7df012a6c94fe1d36da1a83397977860f
ZHShang <zhihengshang@hotmail.com>
2019-06-05 21:50:27 +0000

    Enabled notification for username Change

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,6 @@ import ChatBar from './ChatBar.jsx';
 class App extends Component {
 
 
-  socket = new WebSocket("ws://localhost:3001", "protocolOne");
 
 
   constructor(props){
@@ -18,27 +17,42 @@ class App extends Component {
   }
 
   componentDidMount() {
+    this.socket = new WebSocket("ws://localhost:3001", "protocolOne");
+
 
     this.socket.onopen = e => {
       console.log("connection made");
     }
 
     this.socket.onmessage = ev => {
-          console.log("Received:", ev.data);
-          console.log(JSON.parse(ev.data));
-        }
+      console.log("Received:", ev.data);
+      const msgToShow = JSON.parse(ev.data);
+      switch(msgToShow.type){
+        case "incomingMessage":
+          this.setState((prevState) => {
+            prevState.messages.push(msgToShow);
+            this.setState({messages: prevState.messages});
+          });
+          break;
+        case "incomingNotification":
+          const message ={
+            user: "doNotUse",
+            content: msgToShow.content,
+            id: msgToShow.id
+          }
+          this.setState((prevState) => {
+            prevState.messages.push(message);
+            this.setState({messages: prevState.messages});
+          });
+          break;
+        default:
+          throw new Error("Unknown message type: " + msgToShow.type);
+    }
+
+    }
+
     console.log("componentDidMount <App />");
 
-
-    // setTimeout(() => {
-    //   console.log("Simulating incoming message");
-    //   // Add a new message to the list of messages in the data store
-    //   const newMessage = {id: 7, type:"incomingMessage", username: "Michelle", content: "Hello there!"};
-    //   const messages = this.state.messages.concat(newMessage);
-    //   // Update the state of the app component.
-    //   // Calling setState will trigger a call to render() in App and all child components.
-    //   this.setState({messages: messages})
-    // }, 3000);
   }
 
   handleNewMessage = evt => {
@@ -50,23 +64,23 @@ class App extends Component {
           this.setState({currentUser: oldState.currentUser});
         } else {
           let oldState = this.state;
+          const newMsg = {
+            type: "postNotification",
+            content: oldState.currentUser.name + " has changed their name to " + evt.target.value
+          }
           oldState.currentUser = {name: evt.target.value}
+          this.socket.send(JSON.stringify(newMsg));
           this.setState({currentUser: oldState.currentUser});
           return;
         }
       }
       if(evt.target.className ==="chatbar-message"){
         const nMessage = {
-          id: Math.floor((Math.random() * 100) +1),
-          type: "incomingMessage",
+          type: "postMessage",
           username: this.state.currentUser.name,
           content: evt.target.value
         }
         this.socket.send(JSON.stringify(nMessage));
-
-
-        const messages = this.state.messages.concat(nMessage);
-        this.setState({messages: messages});
         evt.target.value = "";
     }}
 
